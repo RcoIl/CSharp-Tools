cd1c16420ca73881777a91202453b596c76acc7d
RcoIl <rcoil.me@gmail.com>
2019-08-08 23:02:44 +0800

    SharpOfficeInfo

--- README.md
+++ README.md
@@ -4,20 +4,22 @@
 
 列表中的 `.NET 版本` 指的是个人编译环境。
 
-| 项目                   | 描述                                                         | .NET 版本 |
-| ---------------------- | ------------------------------------------------------------ | :-------: |
-| **编码转换工具**       | 编码转换工具                                                 |     4     |
-| **CPing**              | 内网 `C段`存活扫描命令行工具                                 |     4     |
-| **DomainUserList**     | 执行`net user Uname /domain`，限于单域环境                   |     4     |
-| **SharpScreenshot**    | 截图工具（支持多屏）                                         |     4     |
-| **ShellCode_Launcher** | `shellcode` 加载器                                           |     4     |
-| **NavicatCrypto**      | 获取 `Navicat` 的连接记录及保存的账号密码                    |     4     |
-| **WeblogicRCE**        | `Weblogic` 的反序列化系列检测工具                            |     4     |
-| **SharpADDNSDump**     | 查询 `LDAP` 中的 `DNS` 解析记录（`github` 有类似项目）       |     4     |
-| **SharpGetBasisDown**  | 获取机器基础信息（压缩包 `AES` 加密）其中包括高权限 `dump lsass` 内存 |    4.5    |
-| **SharpGetBasisList**  | 解密压缩包，解析 `sqlite` 数据库内容，以获取 `Chrome` 浏览器的浏览、下载记录、密码信息。 |    4.5    |
-| **NetWorkConnectIPC**  | `IPC$` 连接获取远程主机账号列表                              |     4     |
-| **DESDecrypt**         | 批量解密 `DES`加密算法                                       |     4     |
-| **SharpChassisType**   | 用于判断当前机器类型（桌面计算机、笔记本等判断）             |     4     |
-| **SharpDomainSpray**   | 枚举域内用户密码（指定单个密码-枚举，指定单个用户-爆破）     |     4     |
+| 项目                     | 描述                                       | .NET 版本 |
+| ---------------------- | ---------------------------------------- | :-----: |
+| **编码转换工具**             | 编码转换工具                                   |    4    |
+| **CPing**              | 内网 `C段`存活扫描命令行工具                         |    4    |
+| **DomainUserList**     | 执行`net user Uname /domain`，限于单域环境        |    4    |
+| **SharpScreenshot**    | 截图工具（支持多屏）                               |    4    |
+| **ShellCode_Launcher** | `shellcode` 加载器                          |    4    |
+| **NavicatCrypto**      | 获取 `Navicat` 的连接记录及保存的账号密码               |    4    |
+| **WeblogicRCE**        | `Weblogic` 的反序列化系列检测工具                   |    4    |
+| **SharpADDNSDump**     | 查询 `LDAP` 中的 `DNS` 解析记录（`github` 有类似项目）  |    4    |
+| **SharpGetBasisDown**  | 获取机器基础信息（压缩包 `AES` 加密）其中包括高权限 `dump lsass` 内存 |   4.5   |
+| **SharpGetBasisList**  | 解密压缩包，解析 `sqlite` 数据库内容，以获取 `Chrome` 浏览器的浏览、下载记录、密码信息。 |   4.5   |
+| **NetWorkConnectIPC**  | `IPC$` 连接获取远程主机账号列表                      |    4    |
+| **DESDecrypt**         | 批量解密 `DES`加密算法                           |    4    |
+| **SharpChassisType**   | 用于判断当前机器类型（桌面计算机、笔记本等判断）                 |    4    |
+| **SharpDomainSpray**   | 枚举域内用户密码（指定单个密码-枚举，指定单个用户-爆破）            |    4    |
+| **SharpOfficeInfo**    | 查看 `Office` 版本及宏状态                       |    4    |
+| **SharpWebScan**       | 扫描 `C段` 的 `Web 应用`，获取 `Title`，可自定义多端口    |    4    |
 
---
+++ SharpOfficeInfo/SharpOfficeInfo/Program.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SharpOfficeInfo
+{
+    class Program
+    {
+        // 通过注册表检测 Office 是否开启宏
+        private static void OfficeVBAWarnings(string OfficeVersion)
+        {
+            Console.WriteLine();
+            List<string> OfficeFeatures = new List<string>()
+            {
+                "Excel", "Word", "PowerPoint"
+            };
+            foreach (string Features in OfficeFeatures)
+            {
+                string basekey = @"SOFTWARE\Microsoft\Office\" + OfficeVersion + @"\" + Features + @"\Security";
+                RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(basekey);
+                if (registryKey != null)
+                {
+                    string[] ValueNames = registryKey.GetValueNames();
+                    if (registryKey.ValueCount == 0)
+                    {
+                        Console.WriteLine("  [>] {0,-10} 宏状态：禁用所有宏，并发出通知（D）- 2", Features);
+                    }
+                    else
+                    {
+                        foreach (string KeyName in ValueNames)
+                        {
+                            object VBAWarnings = registryKey.GetValue("VBAWarnings");
+                            if (VBAWarnings.ToString() == "1")
+                            {
+                                Console.WriteLine("  [>] {0,-10} 宏状态: 启用所有宏（不推荐；可能会运行有潜在危险的代码）（E）- 1", Features);
+                            }
+                            else if (VBAWarnings.ToString() == "2")
+                            {
+                                Console.WriteLine("  [>] {0,-10} 宏状态：禁用所有宏，并发出通知（D）- 2", Features);
+                            }
+                            else if (VBAWarnings.ToString() == "3")
+                            {
+                                Console.WriteLine("  [>] {0,-10} 宏状态: 禁用无数字签署的所有宏（G）- 3", Features);
+                            }
+                            else if (VBAWarnings.ToString() == "4")
+                            {
+                                Console.WriteLine("  [>] {0,-10} 宏状态：禁用所有宏，并不发出通知（L）- 4", Features);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // 通过注册表检测 Office 版本
+        private static void OfficeIsInstall(string OfficeVersion)
+        {
+            string basekey = @"SOFTWARE\Microsoft\Office\" + OfficeVersion + @"\Common\InstallRoot";
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(basekey);
+            if (registryKey != null)
+            {
+                if (registryKey.GetValue("Path") != null)
+                {
+                    if (OfficeVersion == "8.0")
+                    {
+                        Console.WriteLine("  [>] Microsoft Office Version: Office97");
+                        Console.WriteLine("  [>] Office 安装路径：{0}", registryKey.GetValue("Path"));
+                        OfficeVBAWarnings(OfficeVersion);
+                    }
+                    else if (OfficeVersion == "9.0")
+                    {
+                        Console.WriteLine("  [>] Microsoft Office Version: Office2000");
+                        Console.WriteLine("  [>] Office 安装路径：{0}", registryKey.GetValue("Path"));
+                        OfficeVBAWarnings(OfficeVersion);
+                    }
+                    else if (OfficeVersion == "10.0")
+                    {
+                        Console.WriteLine("  [>] Microsoft Office Version: OfficeXP");
+                        Console.WriteLine("  [>] Office 安装路径：{0}", registryKey.GetValue("Path"));
+                        OfficeVBAWarnings(OfficeVersion);
+                    }
+                    else if (OfficeVersion == "11.0")
+                    {
+                        Console.WriteLine("  [>] Microsoft Office Version: Office2003");
+                        Console.WriteLine("  [>] Office 安装路径：{0}", registryKey.GetValue("Path"));
+                        OfficeVBAWarnings(OfficeVersion);
+                    }
+                    else if (OfficeVersion == "12.0")
+                    {
+                        Console.WriteLine("  [>] Microsoft Office Version: Office2007");
+                        Console.WriteLine("  [>] Office 安装路径：{0}", registryKey.GetValue("Path"));
+                        OfficeVBAWarnings(OfficeVersion);
+                    }
+                    else if (OfficeVersion == "14.0")
+                    {
+                        Console.WriteLine("  [>] Microsoft Office Version: Office2010");
+                        Console.WriteLine("  [>] Office 安装路径：{0}", registryKey.GetValue("Path"));
+                        OfficeVBAWarnings(OfficeVersion);
+                    }
+                    else if (OfficeVersion == "15.0")
+                    {
+                        Console.WriteLine("  [>] Microsoft Office Version: Office2013");
+                        Console.WriteLine("  [>] Office 安装路径：{0}", registryKey.GetValue("Path"));
+                        OfficeVBAWarnings(OfficeVersion);
+                    }
+                    else if (OfficeVersion == "15.0")
+                    {
+                        Console.WriteLine("  [>] Microsoft Office Version: Office2013");
+                        Console.WriteLine("  [>] Office 安装路径：{0}", registryKey.GetValue("Path"));
+                        OfficeVBAWarnings(OfficeVersion);
+                    }
+                    else if (OfficeVersion == "16.0")
+                    {
+                        Console.WriteLine("  [>] Microsoft Office Version: Office2016");
+                        Console.WriteLine("  [>] Office 安装路径：{0}", registryKey.GetValue("Path"));
+                        OfficeVBAWarnings(OfficeVersion);
+                    }
+                }
+            }
+            else
+            {
+                arrayList.Add("  [!] 未安装 Office 软件");
+            }
+        }
+
+        public static ArrayList arrayList = new ArrayList();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            List<string> OfficeVersions = new List<string>()
+            {
+                "8.0", "9.0", "10.0", "11.0", "12.0", "14.0", "15.0", "16.0"
+            };
+            foreach (string OfficeVersion in OfficeVersions)
+            {
+                OfficeIsInstall(OfficeVersion);
+            }
+            if (arrayList.Count == 8)
+            {
+                Console.WriteLine();
+                Console.WriteLine("  [!] 未安装 Office 软件");
+            }
+        }
+    }
+}
